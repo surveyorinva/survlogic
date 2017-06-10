1a680b19b58e90369fb668822e5fbb27276c5f76
chrisfillmore <surveyorinva@gmail.com>
2017-06-10 07:47:15 -0400

    Build with Settings

--- app/src/main/java/com/survlogic/survlogic/activity/AppCompatPreferenceActivity.java
+++ app/src/main/java/com/survlogic/survlogic/activity/AppCompatPreferenceActivity.java
@@ -1,4 +1,4 @@
-package com.survlogic.survlogic;
+package com.survlogic.survlogic.activity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
--- app/src/main/java/com/survlogic/survlogic/activity/SettingsActivity.java
+++ app/src/main/java/com/survlogic/survlogic/activity/SettingsActivity.java
@@ -1,4 +1,4 @@
-package com.survlogic.survlogic;
+package com.survlogic.survlogic.activity;
 
 
 import android.annotation.TargetApi;
@@ -19,6 +19,8 @@ import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
 import android.view.MenuItem;
+
+import com.survlogic.survlogic.R;
 
 import java.util.List;
 
