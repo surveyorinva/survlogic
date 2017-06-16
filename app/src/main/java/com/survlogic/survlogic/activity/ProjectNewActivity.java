a1d1446c4287497e97b43edcc6ebadfc3b0212d1
chrisfillmore <surveyorinva@gmail.com>
2017-06-16 00:04:29 -0400

    v 0.00.03 - New Project UI Established

---
+++ app/src/main/java/com/survlogic/survlogic/activity/ProjectNewActivity.java
@@ -0,0 +1,14 @@
+package com.survlogic.survlogic.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chrisfillmore on 6/12/2017.
+ */
+
+public class ProjectNewActivity extends AppCompatActivity {
+
+
+
+
+}
